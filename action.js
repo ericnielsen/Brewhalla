63698b1ada2b5fe6595a9f1ca5fd01734f8a3ea4
Eric Nielsen <eniel002@gmail.com>
2015-09-18 14:39:11 -0700

    #1 fixed minor errors

--- action.js
+++ action.js
@@ -4,6 +4,7 @@ getList[0].addEventListener('click', function(event) {
     var messageText = 'Breweries located in ' + target.textContent;
     var messageDiv = document.getElementById('message');
     messageDiv.innerHTML = messageText;
+
     var term = event.target.textContent;
     var request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:1337/search/data/' + term);
@@ -11,10 +12,12 @@ getList[0].addEventListener('click', function(event) {
     request.addEventListener('load', function() {
         console.log(request.responseText);
     });
+
 },
 false);
 
-$('a').on('click', function(){
+/* $('a').on('click', function(){
    var target = $(this).attr('rel');
    $("#"+target).show().siblings("div").hide();
 });
+*/
