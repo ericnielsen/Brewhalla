7f2381f0b3ebbdf986ce4d8e17e4250b645d9558
Eric Nielsen <eniel002@gmail.com>
2015-09-21 11:18:40 -0700

    check in

--- action.js
+++ action.js
@@ -13,13 +13,22 @@ getList[0].addEventListener('click', function(event) {
         request.addEventListener('load', function() {
             var getData = JSON.parse(request.responseText);
             //console.log(getData.businesses[0].name);
-
             for(var i = 0; i < getData.businesses.length; i++) {
-                console.log(getData.businesses[i].location);
+                //console.log(getData.businesses[i]);
+                var dataObject = getData.businesses[i];
+                var dataCity = (dataObject.location.city);
+             if (dataCity == term) {
+                //console.log('test')
+                console.log(dataObject.name);
+                console.log(dataObject.location.display_address)
+             }
             }
+
+
             /* var test = getData.businesses[0].name;
              var dataDiv = document.getElementById('datacontent');
              dataDiv.textContent = test; */
         });
     },
     false);
+
