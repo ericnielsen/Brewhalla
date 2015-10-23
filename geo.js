e0820f8b35c349e83db873cab95c319f5ad03630
Eric Nielsen <eniel002@gmail.com>
2015-10-23 13:51:42 -0700

    #6 Functional search using the geolocation parameters operational

--- geo.js
+++ geo.js
@@ -14,9 +14,21 @@ function displayLocation() {
         var request = new XMLHttpRequest();
         request.open('GET', 'http://localhost:1339/location/id/' + lat +'/' +long + '');
         request.send();
-        console.log("latitude: " + position.coords.latitude);
-        console.log("longitude: " + position.coords.longitude);
+        console.log(lat);
+        displayCities();
 
+        function displayCities() {
+            var brewrequest = new XMLHttpRequest();
+            brewrequest.open('GET', 'http://localhost:1339/location/data/'+lat+','+long+'');
+            brewrequest.send();
+            function newRequest(){
+                var getData = JSON.parse(brewrequest.responseText);
+                console.log(getData.businesses[0].name);
+            }
+            brewrequest.addEventListener('load', function(theEvent) {
+                newRequest(theEvent);
+            }, false);
+        }
     }
 }
 window.addEventListener('load', function(event) {
--- location.js
+++ location.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var location=express.Router();
 var mongoose = require('mongoose');
+var yelp=require("node-yelp");
 var Location = mongoose.model('Location', {id: Number, latitude: Number, longitude: Number});
 
 location.get('/:id/:lat/:long',function(req, res){
@@ -20,6 +21,26 @@ location.get('/:id/:lat/:long',function(req, res){
             locationObject.save();
             res.send();
         }
+    });
+});
+
+location.get('/data/:term', function(req,res){
+    var myYelp=yelp.createClient({
+        oauth: {
+            "consumer_key":"XBSJFJsDjNgvoyr2LKzZng",
+            "consumer_secret":"M6MxbbPJ1MMSsnDbFVDxkTPjujk",
+            "token":"h9JSuEFd5WegVN-d1gfwVkpLAR16SPoF",
+            "token_secret":"IYhXjD_5WmksStA3e_46eJOCwBs"
+        }});
+    myYelp.search(
+    {
+        ll: req.params.term,
+        limit:20,
+        sort:1,
+        category_filter:"breweries",
+        radius_filter:"16000"
+        }).then( function(data) {
+         res.send(data);
     });
 });
 module.exports = location;
