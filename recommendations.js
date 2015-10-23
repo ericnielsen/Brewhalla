f5d43b984e99d0337500c717bca88c9303bf534b
Eric Nielsen <eniel002@gmail.com>
2015-10-23 16:10:01 -0700

    Delete recommendations.js

--- recommendations.js
+++
@@ -1,6 +0,0 @@
-var term ='Anaheim';
-var request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:1339/proximity/data/' + term);
-    request.send();
-    var getData = JSON.parse(request.responseText);
-    console.log(getData);
